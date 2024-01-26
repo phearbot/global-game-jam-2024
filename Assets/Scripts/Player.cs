bf490dab16d01d01ea3be12e614991976346f462
Rick <RTSmith801@gmail.com>
2024-01-25 21:46:43 -0700

    Player movement controls

---
+++ Assets/Scripts/Player.cs
@@ -0,0 +1,51 @@
+using UnityEngine;
+
+public class Player : MonoBehaviour
+{
+    public float moveForce = 1.0f;
+    public float maxSpeed = 5.0f;  
+    public float lerpSpeed = 2.0f; 
+    [SerializeField]
+    private float currentSpeed = 0.0f;
+    [SerializeField]
+    Rigidbody rb;
+
+
+    private void Start()
+    {
+        rb = GetComponent<Rigidbody>();
+    }
+
+    void FixedUpdate()
+    {
+        float horizontalInput = Input.GetAxis("Horizontal");
+        float forwardInput = Input.GetAxis("Vertical");
+
+        Move(forwardInput, horizontalInput);
+    }
+
+    public void Move(float forwardInput, float horizontalInput)
+    {
+        // Check if shift is held down for speed boost
+        if (Input.GetKeyDown(KeyCode.LeftShift))
+        {
+            moveForce = 3.0f;
+        }
+
+        if (Input.GetKeyUp(KeyCode.LeftShift))
+        {
+            moveForce = 1.0f;
+        }
+
+        Vector3 forwardForce = new Vector3(0, 0, moveForce * forwardInput);
+        Vector3 horizontalForce = new Vector3(moveForce * horizontalInput, 0, 0);
+        Vector3 forceVector = forwardForce + horizontalForce;
+
+        // Lerp the current speed towards the target speed based on whether there is input
+        float targetSpeed = forceVector.magnitude * maxSpeed;
+        currentSpeed = Mathf.Lerp(currentSpeed, targetSpeed, Time.deltaTime * lerpSpeed);
+
+        // Apply the force using the lerped speed
+        rb.AddForce(forceVector.normalized * currentSpeed, ForceMode.Impulse);
+    }
+}
