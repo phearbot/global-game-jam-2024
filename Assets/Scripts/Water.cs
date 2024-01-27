6d2e1bcb2292424cab02688edf70dfe50777158e
Rick <RTSmith801@gmail.com>
2024-01-26 23:28:07 -0700

    water movement

--- Assets/Scripts/Water.cs
+++ Assets/Scripts/Water.cs
@@ -4,7 +4,21 @@ using UnityEngine;
 using UnityEngine.SceneManagement;
 
 public class Water : MonoBehaviour
-{  
+{
+    public float scrollSpeed = 0.02f;
+    public Renderer rend;
+
+    void Start()
+    {
+        rend = GetComponent<Renderer>();
+    }
+
+    void Update()
+    {
+        float offset = Time.time * scrollSpeed;
+        // water material offset moves (x/y) to add depth to level.
+        rend.material.mainTextureOffset = new Vector2(offset, offset * 0.5f);
+    }
 
     private void OnCollisionEnter(Collision collision)
     {
